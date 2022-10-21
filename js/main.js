cb1642d22b3f8bdd4b6966dbc31e0f3f0d95c086
dora-mandragora <d.byk@list.ru>
2022-10-21 17:41:23 +0300

    replace var

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-var ns = 'http://www.w3.org/2000/svg';
+let ns = 'http://www.w3.org/2000/svg';
 //работает только через ns
 
-var isRect = false;
-var isCircle = false;
+let isRect = false;
+let isCircle = false;
+let isSelect = false;
+let isMove = false;
 
-var isMouseDown = false;
+let isMouseDown = false;
 
 window.onload = () => {   
-    var svgCanvas = document.getElementById('svg');
+    let svgCanvas = document.getElementById('svg');
     const canvas = document.getElementsByClassName('canvas')[0]; 
 
     const rectButton = document.getElementById('rect');
     const circleButton = document.getElementById('circle'); 
     const clearButton = document.getElementById('clear');
-    const emptyButton = document.getElementById('empty');
+    const selectButton = document.getElementById('select');
+    const moveButton = document.getElementById('move');
 
     svgCanvas = createCanvas();
     
@@ -33,29 +36,43 @@ function createCanvas(svgCanvas){
 rectButton.addEventListener('click', ()=>{
     isRect = true;
     isCircle = false;
+    isMove = false;
+    isSelect = false;
 });
 circleButton.addEventListener('click', ()=>{
     isRect = false;
     isCircle = true;
+    isMove = false;
+    isSelect = false;
 });
 
 clearButton.addEventListener('click', () =>{
+    saveFigureProperties();
     canvas.removeChild(svgCanvas);
     createCanvas(svgCanvas);    
 
     svgCanvas = document.getElementById('svg');
 });
 
-emptyButton.addEventListener('click', () =>{
+selectButton.addEventListener('click', () =>{
     isRect = false;
     isCircle = false;
+    isMove = false;
+    isSelect = true;
+});
+
+moveButton.addEventListener('click', () =>{
+    isRect = false;
+    isCircle = false;
+    isMove = true;
+    isSelect = false;
 })
 
 function createRectEvents(e)
 {
     if(!isRect) return;
     isMouseDown = true;
-    var rect = document.createElementNS(ns, 'rect');
+    let rect = document.createElementNS(ns, 'rect');
     rect.setAttributeNS(null, 'width', '1');
     rect.setAttributeNS(null, 'height', '1');
     rect.setAttributeNS(null, 'x', (e.offsetX).toString());
@@ -74,17 +91,17 @@ function createRectEvents(e)
     svgCanvas.appendChild(rect);
     
     function onMouseMoveRect(event) {
-        var status = rect.getAttributeNS(null, 'class');
+        let status = rect.getAttributeNS(null, 'class');
         if(isMouseDown && status == null) moveAtRect(event.offsetX, event.offsetY);           
     }      
     function moveAtRect(dX, dY){
-        var height = dY - shiftY; 
+        let height = dY - shiftY; 
         if(height<0){
             height = -height;
             dY = origLoc.Y - height;
             rect.setAttributeNS(null, 'y', (dY).toString()); 
         }        
-        var width = dX - shiftX;
+        let width = dX - shiftX;
         if(width<0){
             width = -width;
             dX = origLoc.X - width;
@@ -95,8 +112,12 @@ function createRectEvents(e)
     }
     function onMouseUpRect(){
         isMouseDown = false;   
-        rect.setAttributeNS(null, 'class', 'completed');  
-        rect.addEventListener('click', getPropertiesWindow);
+        rect.setAttributeNS(null, 'class', 'completed');
+        if(isSelect)  
+            rect.addEventListener('click', getPropertiesWindow);
+            else rect.removeEventListener('click', getPropertiesWindow);
+
+
     }
     
     };
@@ -105,7 +126,7 @@ function createCircleEvents(e)
 {
     if(!isCircle) return;
     isMouseDown = true;
-    var circle = document.createElementNS(ns,'circle');
+    let circle = document.createElementNS(ns,'circle');
     circle.setAttributeNS(null, 'r', '1');
     circle.setAttributeNS(null, 'cx', (e.offsetX).toString());
     circle.setAttributeNS(null, 'cy', (e.offsetY).toString());
@@ -121,11 +142,11 @@ function createCircleEvents(e)
     svgCanvas.appendChild(circle);
 
     function onMouseMoveCircle(event) {
-        var status = circle.getAttributeNS(null, 'class');
+        let status = circle.getAttributeNS(null, 'class');
         if(isMouseDown && status == null) moveAtCircle(event.offsetX, event.offsetY);           
     }      
     function moveAtCircle(offsetX, offsetY){
-        var radius = Math.sqrt((offsetX-shiftX)**2+
+        let radius = Math.sqrt((offsetX-shiftX)**2+
         (offsetY-shiftY)**2); 
         circle.setAttributeNS(null, 'r', radius.toString());
     }
